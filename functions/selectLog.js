fedc7286c183fafc20dc347364d2ebc6eb14ea79
Daroush Renoit <Daroush.Renoit@ibm.com>
2020-07-22 10:10:52 -0400

    improved error validation for flowlog scan command

--- functions/selectLog.js
+++ functions/selectLog.js
@@ -5,16 +5,20 @@ const readline = require("readline-sync");
 function selectLog(){
     var count=1;
     var files={};
+    var selection=0;
+    console.log("\n");
     fs.readdirSync(logFolder).forEach(file => {
-        files[count]=file;
-        console.log(count+". "+file);
-        count++;
-      });
-      var selection = readline.question(`\nselect log\n`);
-      if(selection>=0 && selection<count){
-        
-        return require("../logs/"+files[selection]);
-      }
-    
+      files[count]=file;
+      console.log(count+". "+file);
+      count++;
+    });
+      do{
+      
+        selection = readline.question(`\nselect log\n`);
+        if(isNaN(selection)|| selection== "" || parseInt(selection)<=0 || parseInt(selection)>=count){
+          console.log("invalid option");
+        }  
+      }while(isNaN(selection)|| selection== "" || parseInt(selection)<=0 || parseInt(selection)>=count);
+      return require("../logs/"+files[selection]);
 }
 module.exports = selectLog;
