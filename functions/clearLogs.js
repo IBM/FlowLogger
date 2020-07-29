932f09ff127ffdcf171b4f660e53b49318b33733
Daroush Renoit <Daroush.Renoit@ibm.com>
2020-07-29 17:07:01 -0400

    added clear logs command

--- functions/clearLogs.js
+++ functions/clearLogs.js
@@ -1,7 +1,22 @@
 const colors = require("colors");
+const fs = require("fs");
 const logFolder = "./logs";
+var problem = false;
 function clearLogs() {
-  console.log("Clearing all files".yellow);
+  if (fs.readdirSync(logFolder).length == 0) {
+    console.log("Folder is already empty\n".yellow);
+    return;
+  }
+  fs.readdirSync(logFolder).forEach(file => {
+    try {
+      fs.unlinkSync(logFolder + "/" + file);
+    } catch (error) {
+      problem = true;
+    }
+  });
+  if (problem) {
+    console.log("Error, returning to home prompt...".red);
+  }
   console.log("All files have been cleared\n".yellow);
 }
 module.exports.clearLogs = clearLogs;
